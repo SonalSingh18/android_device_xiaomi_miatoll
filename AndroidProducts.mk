a05228d8bd1c20d44d0ce53d1acb3b3e64936d62
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-21 17:32:30 +0530

    miatoll: xtended -> havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The Project-Xtended
+# Copyright (C) 2020 The Havoc-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_miatoll.mk
+    $(LOCAL_DIR)/havoc_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_miatoll-user \
-    xtended_miatoll-userdebug \
-    xtended_miatoll-eng
+    havoc_miatoll-user \
+    havoc_miatoll-userdebug \
+    havoc_miatoll-eng
--- havoc_miatoll.mk
+++ havoc_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The Project-Xtended
+# Copyright (C) 2020 The Havoc-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,19 +7,17 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common Xtended stuff
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common Havoc-OS stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-XTENDED_BUILD_TYPE := OFFICIAL
-XTENDED_BUILD_MAINTAINER := SonalSingh
-XTENDED_BUILD_DONATE_URL := https://www.paypal.me/sonal18
-XTENDED_BUILD_SUPPORT_URL := https://t.me/arrow_xtended_miatoll
+HAVOC_BUILD_TYPE := Official
+HAVOC_MAINTAINER := SonalSingh
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := xtended_miatoll
+PRODUCT_NAME := havoc_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
