e7ec4dceb6828a960d0a6e533f464ce6830c0001
Sonal Singh <sonal.singh.19993@gmail.com>
2022-02-23 16:01:37 +0000

    miatoll: arrow -> evolution
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- evolution_miatoll.mk
+++ evolution_miatoll.mk
@@ -7,8 +7,11 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Evolution-X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+TARGET_SUPPORTS_QUICK_TAP := true
+EVO_BUILD_TYPE := CUSTOM
 
 # Inherit lawnchair conditionally
 ifeq ($(LAWNCHAIR), true)
@@ -19,16 +22,13 @@ endif
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := evolution_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
