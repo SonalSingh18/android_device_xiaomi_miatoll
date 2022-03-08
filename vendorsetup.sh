bb4a6bf88115d1d23ea523e47a37f2a22415806c
Sonal Singh <sonal.singh.19993@gmail.com>
2022-03-08 19:58:29 +0000

    miatoll: Add vendorsetup.sh
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+git clone git@github.com:ArrowOS-Devices/android_kernel_xiaomi_sm6250.git -b arrow-12.0 kernel/xiaomi/sm6250
+git clone git@github.com:ArrowOS-Devices/android_device_xiaomi_sm6250-common.git -b arrow-12.0 device/xiaomi/sm6250-common
+git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_miatoll.git -b arrow-12.0 vendor/xiaomi/miatoll
+git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_sm6250-common.git -b arrow-12.0 vendor/xiaomi/sm6250-common
+git clone git@github.com:ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
+git clone git@github.com:SonalSingh18/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.0 packages/apps/GCamGOPrebuilt
+git clone git@github.com:ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.0 hardware/xiaomi
+git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b 12 vendor/xiaomi/miuicamera
+git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
+#git clone --depth=1 https://gitlab.com/SonalSingh18/android_vendor_google.git -b 12.x vendor/google
+#git clone git@github.com:SonalSingh18/Private_tweaks.git -b Quiver packages/apps/Quiver
+#git clone git@github.com:SonalSingh18/Private_tweaks.git -b arrowthemes packages/overlays/Themes
