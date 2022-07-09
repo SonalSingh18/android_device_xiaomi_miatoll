c5d638fb56ae234fbfd72c41024bc5f63a2268eb
Sonal Singh <sonal.singh.19993@gmail.com>
2022-07-09 13:07:05 +0000

    miatoll: Add vendorsetup.sh
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/ArrowOS-Devices/android_kernel_xiaomi_sm6250.git -b arrow-12.1 kernel/xiaomi/sm6250
+git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_sm6250-common.git -b arrow-12.1 device/xiaomi/sm6250-common
+git clone https://github.com/ArrowOS-Devices/android_vendor_xiaomi_miatoll.git -b arrow-12.1 vendor/xiaomi/miatoll
+git clone https://github.com/ArrowOS-Devices/android_vendor_xiaomi_sm6250-common.git -b arrow-12.1 vendor/xiaomi/sm6250-common
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.1 hardware/xiaomi
+git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.1 packages/apps/GCamGOPrebuilt
+git clone https://github.com/SonalSingh18/android_vendor_lawnchair.git -b arrow-12.1 vendor/lawnchair
+git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b 12.1 vendor/xiaomi/miuicamera
+git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
