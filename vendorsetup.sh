20502831938002784a483887cfb2c4dab2a6e7e8
Sonal Singh <sonal.singh.19993@gmail.com>
2023-01-29 13:00:15 +0530

    miatoll: Update dependencies
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/ArrowOS-Devices/android_kernel_xiaomi_sm6250.git -b arrow-13.0 kernel/xiaomi/sm6250
-git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_sm6250-common.git -b arrow-13.0 device/xiaomi/sm6250-common
-git clone https://github.com/ArrowOS-Devices/android_vendor_xiaomi_miatoll.git -b arrow-13.0 vendor/xiaomi/miatoll
-git clone https://github.com/ArrowOS-Devices/android_vendor_xiaomi_sm6250-common.git -b arrow-13.0 vendor/xiaomi/sm6250-common
-git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-13.0 hardware/xiaomi
-git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b master vendor/xiaomi/miuicamera
+git clone https://github.com/SonalSingh18/android_kernel_xiaomi_sm6250.git -b arrow-13.1 kernel/xiaomi/sm6250
+git clone https://github.com/SonalSingh18/android_device_xiaomi_sm6250-common.git -b arrow-13.1 device/xiaomi/sm6250-common
+git clone https://github.com/SonalSingh18/android_vendor_xiaomi_miatoll.git -b arrow-13.1 vendor/xiaomi/miatoll
+git clone https://github.com/SonalSingh18/android_vendor_xiaomi_sm6250-common.git -b arrow-13.1 vendor/xiaomi/sm6250-common
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-13.1 hardware/xiaomi
+git clone https://gitlab.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b thirteen vendor/xiaomi/miuicamera
 git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b.git -b master prebuilts/clang/host/linux-x86/clang-r437112b
