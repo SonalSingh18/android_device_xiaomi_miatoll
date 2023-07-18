106a3cd2d0410d806f21d9254ad813fb5d3a2157
userariii <abhattacharjee717@gmail.com>
2023-07-18 18:04:08 +0000

    miatoll: lineage --> AOSP
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -5,15 +5,28 @@
 #
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_miatoll
+PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := SM6250
+PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+WITH_GMS := true
+
+IS_PHONE := true
+
+# AOSP Stuff
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ACORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_USES_AOSP_RECOVERY := true
+CUSTOM_BUILD_TYPE := COMMUNITY-edition
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="miatoll_global-user 12 RKQ1.211019.001 V13.0.2.0.SJWMIXM release-keys"
