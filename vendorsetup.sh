3e01e7069a8981e1f559d03b1a507dc680854b89
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-08-03 15:42:14 +0530

    miatoll: Init vendorsetup.sh
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# device/xiaomi
+git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_device_xiaomi_sm6250-common.git device/xiaomi/sm6250-common --depth=1
+
+# vendor/xiaomi
+git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
+git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_sm6250-common.git vendor/xiaomi/sm6250-common --depth=1
+git clone -b miuicam https://gitlab.com/userariii/vendor-xiaomi-miuicamera.git vendor/xiaomi/miuicamera --depth=1
+git clone -b A13 https://github.com/userariii/hardware_dolby.git hardware/dolby --depth=1
+
+# clang
+git clone -b main https://gitlab.com/Panchajanya1999/azure-clang prebuilts/clang/host/linux-x86/clang-14.0 --depth=1
+
+# kernel/xiaomi
+git clone -b Forza-13 https://github.com/Xiaomi-SD720G-Devices/android_kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1
