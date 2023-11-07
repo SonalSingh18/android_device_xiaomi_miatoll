44331410affc6b7009913bb8e98ccd2628417ae0
userariii <abhattacharjee717@gmail.com>
2023-11-07 21:22:25 +0000

    miatoll: Track A14 repos
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 # device/xiaomi
-git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_device_xiaomi_sm6250-common.git device/xiaomi/sm6250-common --depth=1
+git clone -b 14 https://github.com/Xiaomi-SD720G-Devices/android_device_xiaomi_sm6250-common.git device/xiaomi/sm6250-common --depth=1
 
 # vendor/xiaomi
 git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
-git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_sm6250-common.git vendor/xiaomi/sm6250-common --depth=1
+git clone -b 14 https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_sm6250-common.git vendor/xiaomi/sm6250-common --depth=1
 git clone -b miuicam https://gitlab.com/userariii/vendor-xiaomi-miuicamera.git vendor/xiaomi/miuicamera --depth=1
 git clone -b main https://github.com/userariii/hardware_dolby.git hardware/dolby --depth=1
 
